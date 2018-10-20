2cc0610a20383a0b3214074b1fb1280753883e93
emelyanovIgor <strike2002@ukr.net>
2018-10-20 12:13:58 +0300

    finished watching video. mod13

--- js_node.js_13/main.js
+++ js_node.js_13/main.js
@@ -19,6 +19,7 @@ Update - put
 Delete - delete*/
 //***************************************
 
+/*
 const http = require('http');
 const secret = require('./secret');
 
@@ -35,3 +36,39 @@ http.createServer((req, res) => {
 }).listen(port, pathName, () => {
   console.log('...start listening.')
 });
+*/
+
+const http = require('http');
+const secret = require('./secret');
+
+const port = 3000;
+const pathName = '127.0.0.1';
+
+const stud = JSON.parse('{"students": ["student1", "student2", "student3"]}');
+http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    if (req.url === '/admin') {
+      res.write('HELLO ADMIN!');
+    }
+    if (req.url === '/students') {
+      stud.students.forEach((student) => {
+        res.write(student + '\n');
+      })
+
+    }
+  }
+  if (req.method === 'POST'){
+let postDate = '';
+
+req.on('data', (chunk) => {
+  postDate += chunk;
+});
+req.on('end', () => {
+  console.log(postDate);
+})
+  }
+  res.end();
+}).listen(port, pathName, () => {
+  console.log('...start listening')
+});
+
